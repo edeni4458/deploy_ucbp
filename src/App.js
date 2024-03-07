ebe399a2d82acd5bb351875b62ea218d9bfac3be
Emmanuel <edeni4458@gmail.com>
2024-03-07 15:45:11 -0500

    Fourth Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css"
 
 import Connect from './components/MainBarTags/connect/Connect';
 import CBnews from './components/MainBarTags/cbNews/CBnews';
 import About from './components/MainBarTags/about/About';
 import Create from './components/MainBarTags/create/Create';
-import View from './components/MainBarTags/view/View';
 import Spotify from './components/MainBarTags/ucbpEpisodes/Spotify';
 import UCBPepisodes from './components/MainBarTags/ucbpEpisodes/UCBPepisodes';
 import ThanksModal from './components/MainBarTags/modals/ThanksModal';
@@ -25,15 +24,14 @@ function App() {
     <div className="App">
       <MainBar></MainBar>
       <Routes>
-        <Route exact path="/deploy_ucbp" element={<MainPage/>}/>
-        <Route exact path="/view" element={<View />} />
-        <Route exact path='/create' element={<Create/>} />
-        <Route exact path='/about' element={<About/>} />
-        <Route exact path='/connect' element={<Connect/>} />
-        <Route exact path='/news' element={<CBnews/>} />
-        <Route exact path='/episodes' element={<Spotify/>} />
-        <Route exact path='/UCBP' element={<UCBPepisodes/>} />
-        <Route exact path='/thankyou' element={<ThanksModal/>}/>
+        <Route exact path="/deploy_ucbp" element={<MainPage />} />
+        <Route exact path='/create' element={<Create />} />
+        <Route exact path='/about' element={<About />} />
+        <Route exact path='/connect' element={<Connect />} />
+        <Route exact path='/news' element={<CBnews />} />
+        <Route exact path='/episodes' element={<Spotify />} />
+        <Route exact path='/UCBP' element={<UCBPepisodes />} />
+        <Route exact path='/thankyou' element={<ThanksModal />} />
       </Routes>
       <BotBar></BotBar>
     </div>
